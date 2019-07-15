9ae1a61d4edd1759df2114dd1c7e1a43796c6a3e
TaraBC <TaraBC@users.noreply.github.com>
2019-07-15 19:14:37 +0100

    race, background and class lists made

--- Scripts/randomCharacter.js
+++ Scripts/randomCharacter.js
@@ -1,8 +1,40 @@
+/*All Races in Lists, separated by pack*/
+basicRaces=['Dragonborn','Dwarf','Elf','Gnome','Half-Elf','Halfling','Half-Orc','Human','Tiefling'];
+elementalEvilRaces=['Arakocra','Genasi','Goliath'];
+volosMonsterRaces=['Aasimar','Bugbear','Firbolg','Goblin','Hobgoblin','Kenku','Kobold','Lizardfolk','Orc','Tabaxi','Triton','Yuan-ti Pureblood'];
+swordCoastRaces=['Feral Tiefling'];
+tortleRaces=['Tortle'];
+mordenkainenRaces=['Gith'];
+eberronRaces=['Changeling','Kalashtar','Shifter','Warforged'];
+ravnicaRaces=['Centaur','Loxodon','Minotaur','Simic Hybrid','Vedalken'];
+acquisitionsRaces=['Verdan'];
+allRaces= basicRaces.concat(elementalEvilRaces,volosMonsterRaces,swordCoastRaces,tortleRaces,mordenkainenRaces,eberronRaces,ravnicaRaces,acquisitionsRaces);
+
+/*All classes in Lists separated by pack*/
+basicClasses=['Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard'];
+unearthedArcanaClasses=['Artificer'];
+critRoleClasses=['Blood Hunter'];
+allClasses= basicClasses.concat(unearthedArcanaClasses,critRoleClasses);
+
+/*All backgrounds in Lists separated by pack*/
+acquisitionsBacks=['Celebrity Adventurers Scion','Failed Merchant','Gambler','Plaintiff','Rival Intern']
+basicBacks=['Acolyte','Charlatan','Criminal/Spy','Entertainer','Folk Hero','Gladiator','Guild Aritsan/Merchant','Hermit','Knight','Noble','Outlander','Pirate','Sage','Sailor','Soldier','Urchin']
+strahdBacks=['Haunted Ones']
+saltmarshBacks=['Fisher','Marine','Shipwright','Smuggler']
+ravnicaBacks=['Azorius Functionary','Boros Legionnaire','Dimir Operative','Golgari Agent','Gruul Anarch','Izzet Engineer','Orzhov Representative','Rakdos Cultist','Selesnya Initiate','Simic Scientist']
+swordCoastBacks=['City Watch/Investigator','Clan Crafter','Cloistered Scholar','Courtier','Faction Agent','Far Traveler','Inheritor','Knight Of The Order','Mercenary Veteran','Urban Bounty Hunter','Uthgardt Tribe Member','Waterdhavian Noble']
+tombBacks=['Anthropologist','Archaeologist']
+eberronBacks=['Cannith House Agent','Deneith House Agent','Ghallanda House Agent','Jorasco House Agent','Kundarak House Agent','Lyrandar House Agent','Medani House Agent','Orien House Agent','Phiarlan House Agent','Sivis House Agent','Tharashk House Agent','Thuranni House Agent','Vadalis House Agent']
+allBacks=acquisitionsBacks.concat(basicBacks,saltmarshBacks,strahdBacks,ravnicaBacks,swordCoastBacks,tombBacks,eberronBacks)
+
+/*Simple Generation*/
 function simpleGeneration(form){
 	playerName=form.player.value;
 	newCharacter= new character(playerName);
+	alert(allRaces);
 }
 
+/*D&D character class*/
 class character{
 	constructor(playerName){
 	/*Background info*/
