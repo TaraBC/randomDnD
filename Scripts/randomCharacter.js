dba03bf528205361290381186dce3717aefae148
TaraBC <TaraBC@users.noreply.github.com>
2019-07-17 19:44:59 +0100

    Updated randomCharacter.js
    - changed to javascript syntax, mistakes made earlier
    - randomRace method added

--- Scripts/randomCharacter.js
+++ Scripts/randomCharacter.js
@@ -1,45 +1,56 @@
+'use strict';
+
 /*All Races in Lists, separated by pack*/
-basicRaces=['Dragonborn','Dwarf','Elf','Gnome','Half-Elf','Halfling','Half-Orc','Human','Tiefling'];
-elementalEvilRaces=['Arakocra','Genasi','Goliath'];
-volosMonsterRaces=['Aasimar','Bugbear','Firbolg','Goblin','Hobgoblin','Kenku','Kobold','Lizardfolk','Orc','Tabaxi','Triton','Yuan-ti Pureblood'];
-swordCoastRaces=['Feral Tiefling'];
-tortleRaces=['Tortle'];
-mordenkainenRaces=['Gith'];
-eberronRaces=['Changeling','Kalashtar','Shifter','Warforged'];
-ravnicaRaces=['Centaur','Loxodon','Minotaur','Simic Hybrid','Vedalken'];
-acquisitionsRaces=['Verdan'];
-allRaces= basicRaces.concat(elementalEvilRaces,volosMonsterRaces,swordCoastRaces,tortleRaces,mordenkainenRaces,eberronRaces,ravnicaRaces,acquisitionsRaces);
+var basicRaces=['Dragonborn','Dwarf','Elf','Gnome','Half-Elf','Halfling','Half-Orc','Human','Tiefling'];
+var elementalEvilRaces=['Arakocra','Genasi','Goliath'];
+var volosMonsterRaces=['Aasimar','Bugbear','Firbolg','Goblin','Hobgoblin','Kenku','Kobold','Lizardfolk','Orc','Tabaxi','Triton','Yuan-ti Pureblood'];
+var swordCoastRaces=['Feral Tiefling'];
+var tortleRaces=['Tortle'];
+var mordenkainenRaces=['Gith'];
+var eberronRaces=['Changeling','Kalashtar','Shifter','Warforged'];
+var ravnicaRaces=['Centaur','Loxodon','Minotaur','Simic Hybrid','Vedalken'];
+var acquisitionsRaces=['Verdan'];
+var allRaces= basicRaces.concat(elementalEvilRaces,volosMonsterRaces,swordCoastRaces,tortleRaces,mordenkainenRaces,eberronRaces,ravnicaRaces,acquisitionsRaces);
 
 /*All classes in Lists separated by pack*/
-basicClasses=['Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard'];
-unearthedArcanaClasses=['Artificer'];
-critRoleClasses=['Blood Hunter'];
-allClasses= basicClasses.concat(unearthedArcanaClasses,critRoleClasses);
+var basicClasses=['Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin','Ranger','Rogue','Sorcerer','Warlock','Wizard'];
+var unearthedArcanaClasses=['Artificer'];
+var critRoleClasses=['Blood Hunter'];
+var allClasses= basicClasses.concat(unearthedArcanaClasses,critRoleClasses);
 
 /*All backgrounds in Lists separated by pack*/
-acquisitionsBacks=['Celebrity Adventurers Scion','Failed Merchant','Gambler','Plaintiff','Rival Intern'];
-basicBacks=['Acolyte','Charlatan','Criminal/Spy','Entertainer','Folk Hero','Gladiator','Guild Aritsan/Merchant','Hermit','Knight','Noble','Outlander','Pirate','Sage','Sailor','Soldier','Urchin'];
-strahdBacks=['Haunted Ones'];
-saltmarshBacks=['Fisher','Marine','Shipwright','Smuggler'];
-ravnicaBacks=['Azorius Functionary','Boros Legionnaire','Dimir Operative','Golgari Agent','Gruul Anarch','Izzet Engineer','Orzhov Representative','Rakdos Cultist','Selesnya Initiate','Simic Scientist'];
-swordCoastBacks=['City Watch/Investigator','Clan Crafter','Cloistered Scholar','Courtier','Faction Agent','Far Traveler','Inheritor','Knight Of The Order','Mercenary Veteran','Urban Bounty Hunter','Uthgardt Tribe Member','Waterdhavian Noble'];
-tombBacks=['Anthropologist','Archaeologist'];
-eberronBacks=['Cannith House Agent','Deneith House Agent','Ghallanda House Agent','Jorasco House Agent','Kundarak House Agent','Lyrandar House Agent','Medani House Agent','Orien House Agent','Phiarlan House Agent','Sivis House Agent','Tharashk House Agent','Thuranni House Agent','Vadalis House Agent'];
-allBacks=acquisitionsBacks.concat(basicBacks,saltmarshBacks,strahdBacks,ravnicaBacks,swordCoastBacks,tombBacks,eberronBacks);
+var acquisitionsBacks=['Celebrity Adventurers Scion','Failed Merchant','Gambler','Plaintiff','Rival Intern'];
+var basicBacks=['Acolyte','Charlatan','Criminal/Spy','Entertainer','Folk Hero','Gladiator','Guild Aritsan/Merchant','Hermit','Knight','Noble','Outlander','Pirate','Sage','Sailor','Soldier','Urchin'];
+var strahdBacks=['Haunted Ones'];
+var saltmarshBacks=['Fisher','Marine','Shipwright','Smuggler'];
+var ravnicaBacks=['Azorius Functionary','Boros Legionnaire','Dimir Operative','Golgari Agent','Gruul Anarch','Izzet Engineer','Orzhov Representative','Rakdos Cultist','Selesnya Initiate','Simic Scientist'];
+var swordCoastBacks=['City Watch/Investigator','Clan Crafter','Cloistered Scholar','Courtier','Faction Agent','Far Traveler','Inheritor','Knight Of The Order','Mercenary Veteran','Urban Bounty Hunter','Uthgardt Tribe Member','Waterdhavian Noble'];
+var tombBacks=['Anthropologist','Archaeologist'];
+var eberronBacks=['Cannith House Agent','Deneith House Agent','Ghallanda House Agent','Jorasco House Agent','Kundarak House Agent','Lyrandar House Agent','Medani House Agent','Orien House Agent','Phiarlan House Agent','Sivis House Agent','Tharashk House Agent','Thuranni House Agent','Vadalis House Agent'];
+var allBacks=acquisitionsBacks.concat(basicBacks,saltmarshBacks,strahdBacks,ravnicaBacks,swordCoastBacks,tombBacks,eberronBacks);
+
+
+/*Dice arrays*/
+var D6 = Array(1,2,3,4,5,6);
+
+/*addition for reduce*/
+function addition(total,num){
+	return total+num;
+}
 
 /*Simple Generation*/
 
 function simpleGeneration(form){
-	playerName=form.player.value;
-	if (playerName=''){
+	var playerName=form.player.value;
+	if (!playerName){
 		alert("Please enter a player name");
 	}
 	else{
 		
-	newCharacter= new character(playerName);
+	let newCharacter= new character(playerName);
 	newCharacter.lvl=1;
-	newCharacter.randomRace(newCharacter); 
-	newCharacter.abilityScores(newCharacter);
+	newCharacter.randomRace([]); 
+	newCharacter.abilityScores();
 	}
 }
 
@@ -48,10 +59,10 @@ function simpleGeneration(form){
 class character{
 	constructor(playerName){
 	/*Background info*/
-	this.name='';
-	this.Class='';
-	this.background='';
-	this.lvl='';
+	this.name;
+	this.Class;
+	this.background;
+	this.lvl;
 	this.playerName=playerName;
 	this.race='';
 	this.alignment='';
@@ -112,57 +123,66 @@ class character{
 	
 	/*METHOD: CALCULATE AND ASSIGN ABILITY SCORES*/
 	abilityScores(){
-	diceList=[];
-	/*for all 6 attributes*/
-	for (i=0;i<6;i++){
-		/*Create a temporary list of 4 numbers between 1 and 6*/
-		tempList=[]
-		for (j=0;j<4;i++){
-			tempList.push(Math.floor(Math.random()*6));
+	var diceList=[null,null,null,null,null,null];
+	for (var i=0;i<6;i++){
+		var tempList=new Array(null,null,null,null);
+		for (var j=0;j<4;j++){
+			tempList[j]=D6[Math.floor(Math.random()*6)];
 		}
-		/*Find and remove minimum*/
-		min=Math.min(...tempList);
-		found=false;
-		k=0;
-		sum=0;
-		while (found=false){
+		var min=Math.min(...tempList);
+		var found=false;
+		var k=0;
+		while (found==false){
+			k+=1
+			console.log(k);
 			if (tempList[k]=min){
-				tempList.splice(k,1);
-				found=true
+				found=true;
 			}
 			
 		}
-		/*Find sum of temporary numbers*/
-		for (l=0;l<3;l++){
-			sum+=tempList[l];
-		}
-		/*Push sum of temporary numbers to list of 6 attribute numbers*/
-		diceList.push(sum);
+		diceList[i]=tempList.reduce(addition);
 	}
 	
+	
 	/*Assign all ability attributes*/
-	if (this.str = ''){	
+	if (this.str==''){
 		this.str=diceList[0];
 	}
-	this.strmod=modCalc(this.str);
-	if (this.dex = ''){	
+	this.strmod=this.modCalc(this.str);
+	if (this.dex == ''){	
 	this.dex=diceList[1];
 	}
-	this.dexmod=modCalc(this.dex);
-	if (this.con = ''){	
+	this.dexmod=this.modCalc(this.dex);
+	if (this.con == ''){	
 	this.con=diceList[2];
 	}
-	this.conmod=modCalc(this.con);
+	this.conmod=this.modCalc(this.con);
+	if (this.intelligence==''){
 	this.intelligence=diceList[3];
-	this.intmod=modCalc(this.intelligence);
+	}
+	this.intmod=this.modCalc(this.intelligence);
+	if (this.wis==''){
 	this.wis=diceList[4];
-	this.wismod=modCalc(this.wis);
+	}
+	this.wismod=this.modCalc(this.wis);
+	if (this.cha==''){
 	this.cha=diceList[5];
-	this.chamod=modCalc(this.cha);
+	}
+	this.chamod=this.modCalc(this.cha);
 	}
 	
 	/*METHOD: CALCULATE MODIFIERS*/
 	modCalc(score){
 		return ((score)/2)-5;
+	}
+	
+	/*Chooses random race*/
+	/*preferences is an array of chosen packs*/
+	randomRace(preferences){
+		if (this.race==''){
+			if (preferences=[]){
+				this.race= allRaces[Math.floor(Math.random()*allRaces.length)];
+			}
+		}
 	}
 }
