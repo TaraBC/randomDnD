3f8beeb08ce3362f88ebce79eb3a1205fc5c19cd
TaraBC <TaraBC@users.noreply.github.com>
2019-07-24 18:37:22 +0100

    Updated randomCharacter.js
    - creation of races defined in subclasses as opposed to methods of the character class
    - Dwarf class completed
    - dragonborn class completed
    - random naming finished
    - random aging finished

--- Scripts/randomCharacter.js
+++ Scripts/randomCharacter.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*All Races in Lists, separated by pack*/
-const basicRaces = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling'];
+const basicRaces = ['Dragonborn', 'Hill Dwarf','Mountain Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling'];
 const elementalEvilRaces = ['Arakocra', 'Genasi', 'Goliath'];
 const volosMonsterRaces = ['Aasimar', 'Bugbear', 'Firbolg', 'Goblin', 'Hobgoblin', 'Kenku', 'Kobold', 'Lizardfolk', 'Orc', 'Tabaxi', 'Triton', 'Yuan-ti Pureblood'];
 const swordCoastRaces = ['Feral Tiefling'];
@@ -29,6 +29,9 @@ const tombBacks=['Anthropologist','Archaeologist'];
 const eberronBacks=['Cannith House Agent','Deneith House Agent','Ghallanda House Agent','Jorasco House Agent','Kundarak House Agent','Lyrandar House Agent','Medani House Agent','Orien House Agent','Phiarlan House Agent','Sivis House Agent','Tharashk House Agent','Thuranni House Agent','Vadalis House Agent'];
 const allBacks=acquisitionsBacks.concat(basicBacks,saltmarshBacks,strahdBacks,ravnicaBacks,swordCoastBacks,tombBacks,eberronBacks);
 
+/*basic arrays*/
+const genders=['Male','Female'];
+const truths=[true,false]
 
 /*Dice arrays*/
 const D6 = Array(1,2,3,4,5,6);
@@ -45,7 +48,7 @@ function addition(total,num){
 	return total+num;
 }
 
-/*Simple Generation*/
+////////*SIMPLE GENERATION*////////
 
 function simpleGeneration(form){
 	const playerName = form.player.value;
@@ -54,108 +57,152 @@ function simpleGeneration(form){
 	}
 	else{
 		
-	let newCharacter= new character(playerName,[],true,true); //character initialisation
+	let newCharacter= raceAssign('Dragonborn',true,'Tara'); //character initialisation
+	console.log(newCharacter.name,newCharacter.gender);
+	}
+
+}
+
+
+//////RANDOM RACE ASSIGNMENT/////////
+function randomRace(DLCpreferences,adultPreference,playerName) {
+	let race;
+	if (DLCpreferences === []) {
+		race = allRaces[Math.floor(Math.random() * allRaces.length)];		//Pick races
+	}
+
+	return raceAssign(race,adultPreference,playerName);
+}
+
+
+/*Assign values based on race*/
+
+
+function raceAssign(race,adultPreference,playerName){
+	if (race==='Dragonborn'){
+		return new Dragonborn(playerName,adultPreference,true);
+	}
+	else if  (race==='Hill Dwarf'){
+		return new hillDwarf(playerName,adultPreference,true);
+	}
+	else if (race==='Mountain Dwarf'){
+		return new mountainDwarf(playerName,adultPreference,true);
+	}
+	else if (race==='Elf'){
+
+	}
+	else if(race==='Gnome'){
+
+	}
+	else if (race==='Half-Elf'){
+
+	}
+	else if(race==='Halfling'){
+
+	}
+	else if (race==='Half-Orc'){
+
+	}
+	else if (race==='Human'){
+
+	}
+	else if (race==='Tiefling'){
+
 	}
 }
 
 
+///////RANDOM CLASS ASSIGNMENT//////
+function randomClass(DLCpreferences){
+	if (this.Class===undefined){
+		if (DLCpreferences === []){
+			this.Class=allClasses[Math.floor(Math.random()*allClasses.length)];
+		}
+	}
+}
+
 /*D&D character class*/
 class character{
 	/*Background info*/
-	name = '';
-	Class='';
-	background='';
-	lvl='';
-	race='';
-	alignment='';
-	personalityTrait='';
-	ideal='';
-	bonds='';
-	flaws='';
-	size='';
-	age='';
+	name;
+	Class;
+	background;
+	lvl;
+	race;
+	alignment;
+	personalityTrait;
+	ideal;
+	bonds;
+	flaws;
+	size;
+	age;
+	gender;
 	/*Ability Scores*/
-	str='';
-	strmod='';
-	dex='';
-	dexmod='';
-	con='';
-	conmod='';
-	intelligence='';
-	intmod='';
-	wis='';
-	wismod='';
-	cha='';
-	chamod='';
-	profBonus='';
+	str;
+	strmod;
+	dex;
+	dexmod;
+	con;
+	conmod;
+	intelligence;
+	intmod;
+	wis;
+	wismod;
+	cha;
+	chamod;
+	profBonus;
 	/*Ability Modifiers*/
-	savingStr='';
-	savingDex='';
-	savingCon='';
-	savingInt='';
-	savingWis='';
-	savingCha='';
-	acrobatics='';
-	animalHandling='';
-	deception='';
-	History='';
-	insight='';
-	intimidation='';
-	investigation='';
-	medicine='';
-	nature='';
-	perception='';
-	performance='';
-	persuasion='';
-	religion='';
-	sleightOfHand='';
-	stealth='';
-	survival='';
+	savingStr;
+	savingDex;
+	savingCon;
+	savingInt;
+	savingWis;
+	savingCha;
+	acrobatics;
+	animalHandling;
+	deception;
+	History;
+	insight;
+	intimidation;
+	investigation;
+	medicine;
+	nature;
+	perception;
+	performance;
+	persuasion;
+	religion;
+	sleightOfHand;
+	stealth;
+	survival;
 	/*Fight info*/
-	AC='';
-	initiative='';
-	speed='';
-	maxHP='';
-	hitDice='';
+	AC;
+	initiative;
+	speed;
+	maxHP;
+	hitDice;
+	house;
 	/*proficiencies and languages*/
 	proficiencies=[[],[]];
 	languages=[];
 	/*Features and traits*/
 	features=[];
-	constructor(playerName,DLCPreferences,completeRandom,adultPreference){
-		this.playerName=playerName;
-        this.abilityScores();
-
-        if (completeRandom===true){
-			this.lvl=1;
-			this.profBonus=2;
+	constructor(playerName,completeRandom,preferredName,preferredGender) {
+		this.playerName = playerName;
+		if (preferredName !== undefined){
+			this.name=preferredName;
 		}
 
-		if (this.race==='') {
-			this.randomRace(DLCPreferences,adultPreference);
+		if (preferredGender!==undefined){
+			this.gender=preferredGender;
+		}
+		else{
+			this.gender=genders[Math.floor(Math.random()*genders.length)];
 		}
 
-		if (this.class==='') {
-			this.randomClass(DLCPreferences);
+		if (completeRandom === true) {
+			this.lvl = 1;
+			this.profBonus = 2;
 		}
-
-        if (this.alignment===''){
-            const alignment1 = alignmentType[Math.floor(Math.random()*alignmentType.length)];
-            const alignment2 = alignmentType2[Math.floor(Math.random()*alignmentType2.length)];
-            if (alignment1===alignment2){
-                this.alignment='True Neutral';
-            }
-            else{
-                alignment1.concat(' ',alignment2);
-            }
-        }
-
-		this.strmod=this.modCalc(this.str);
-		this.dexmod=this.modCalc(this.dex);
-		this.conmod=this.modCalc(this.con);
-		this.intmod=this.modCalc(this.intelligence);
-		this.wismod=this.modCalc(this.wis);
-		this.chamod=this.modCalc(this.cha);
 	}
 	
 	/*METHOD: CALCULATE AND ASSIGN ABILITY SCORES*/
@@ -178,22 +225,22 @@ class character{
 	
 	
 	/*Assign all ability attributes*/
-	if (this.str===''){
+	if (this.str===undefined){
 		this.str=diceList[0];
 	}
-	if (this.dex === ''){
+	if (this.dex === undefined){
 	this.dex=diceList[1];
 	}
-	if (this.con === ''){
+	if (this.con === undefined){
 	this.con=diceList[2];
 	}
-	if (this.intelligence===''){
+	if (this.intelligence===undefined){
 	this.intelligence=diceList[3];
 	}
-	if (this.wis===''){
+	if (this.wis===undefined){
 	this.wis=diceList[4];
 	}
-	if (this.cha===''){
+	if (this.cha===undefined){
 	this.cha=diceList[5];
 	}
 	}
@@ -202,96 +249,128 @@ class character{
 	modCalc(score){
 		return Math.floor(((score)/2)-5);
 	}
-	
-	/*Chooses random race*/
-	/*preferences is an array of chosen packs*/
-	randomRace(DLCpreferences,adultPreference){
-		if (this.race==='') {
-			if (DLCpreferences === []) {
-				this.race = allRaces[Math.floor(Math.random() * allRaces.length)];		//Pick races
-			}
-		}
-		/*Assign values based on race*/
-		if (this.race==='Dragonborn'){
-			this.createDragonborn(adultPreference);
-		}
-		else if  (this.race==='Dwarf'){
-		    this.createDwarf(adultPreference);
-        }
-		else if (this.race==='Elf'){
 
-        }
-		else if(this.race==='Gnome'){
+	/*Assign random alignment*/
 
-        }
-		else if (this.race==='Half-Elf'){
 
-        }
-		else if(this.race==='Halfling'){
-
-        }
-		else if (this.race==='Half-Orc'){
-
-        }
-		else if (this.race==='Human'){
-
-        }
-		else if (this.race==='Tiefling'){
-
-        }
-	}
-
-	randomClass(preferences){
-		if (this.Class===''){
-			if (preferences === []){
-				this.Class=allClasses[Math.floor(Math.random()*allClasses.length)];
+    randomAlignment() {
+		if (this.alignment === undefined) {
+			const alignment1 = alignmentType[Math.floor(Math.random() * alignmentType.length)];
+			const alignment2 = alignmentType2[Math.floor(Math.random() * alignmentType2.length)];
+			if (alignment1 === alignment2) {
+				this.alignment = 'True Neutral';
+			} else {
+				alignment1.concat(' ', alignment2);
 			}
 		}
 	}
 
-	//*Different race functions*//
-
-	createDragonborn(adultPreference,firePreference){
-		this.str+=2;
-		this.cha+=1;
-		this.size='Medium';
-		this.speed=30;
-		this.features.push('Draconic Ancestry','Breath Weapon','Damage Resistance');
-		this.languages.push('Common','Draconic');
-		if (!firePreference){
-			this.fireType=dragonbornFires[Math.floor(Math.random()*dragonbornFires.length)];
+	randomAge(adultPreference,maxAge,minAdultAge,minChildAge){
+    	if (adultPreference===true){
+    		this.age=Math.floor(Math.random()*(maxAge-minAdultAge)+minAdultAge);
 		}
-		if (this.age===''){
-			if (adultPreference===true){
-				this.age=Math.floor(Math.random()*(81-14)+15); //random number between 15 and 81
+    	else{
+    		this.age=Math.floor(Math.random()*(maxAge-minAdultAge)+minChildAge);
+		}
+	}
+
+
+}
+
+//////RACE SUBCLASS//////
+class Race extends character{
+
+	raceHP=0;
+	raceFeatures=[];
+	raceLanguages=[];
+	raceProficiencies=[[],[]];			//workAround for proficiencies,languages and features
+	childNames=[];
+	maleNames=[];
+	femaleNames=[];					//List of potential names
+	clanNames=[];
+	constructor(playerName,completeRandom){
+		super(playerName,completeRandom);
+		this.randomAlignment();
+		this.abilityScores();
+	}
+	randomName(adultAge){				//random name picker, if child names are not applicable, input 0 as adultAge
+		let fName;
+		let lName;
+		if (this.name===undefined){
+			if (this.age>adultAge) {
+				if (this.gender === 'Female') {
+					fName = this.femaleNames[Math.floor(Math.random() * this.femaleNames.length)];
+				} else if (this.gender === 'Male') {
+					fName = this.maleNames[Math.floor(Math.random() * this.maleNames.length)];
+				}
 			}
 			else{
-				this.age=Math.floor(Math.random()*(81-2)+3);
+				fName=this.childNames[Math.floor(Math.random() * this.childNames.length)];
 			}
+			lName=this.clanNames[Math.floor(Math.random()*this.clanNames.length)];
+			this.name= fName.concat(' ',lName);
 		}
 	}
+}
 
-	createDwarf(adultPreference,toolPreference){
-        this.con+=2;
-        this.size='Medium';
-        this.speed=25;
-        this.features.push('Darkvision','Dwarven Resilience','Stonecunning');
-        this.proficiencies[1].push('Battleaxe','Handaxe','Light Hammer','Warhammer');
-        this.languages.push('Common','Dwarvish');
-        if (!toolPreference){
-            const tools=["Smith's tools","Brewer's Supplies","Mason's Tools"];
-            this.proficiencies[0].push(tools[Math.floor(Math.random()*tools.length)]);
-        }
-        else{
-            this.proficiencies[0].push(toolPreference);
-        }
-        if (adultPreference===true){
-            this.age=Math.floor(Math.random()*((350-18)+18));
-        }
-        else{
-            this.age=Math.floor(Math.random()*((350-10)+10))
-        }
-    }
+//////SUBCLASSES FOR RACE///////
+
+class Dragonborn extends Race{
+	fireType;
+	constructor(playerName,adultPreference,completeRandom,firePreference) {
+		super(playerName,completeRandom);
+		this.maleNames.push('Arjhan', 'Balasar', 'Bharash', 'Donaar', 'Ghesh', 'Heskan', 'Kriv', 'Medrash', 'Mehen', 'Nadarr', 'Pandjed', 'Patrin', 'Rhogar', 'Shamash', 'Shedinn', 'Tarhun', 'Torinn');
+		this.femaleNames.push('Akra', 'Biri', 'Daar', 'Farideh', 'Harann', 'Havilar', 'Jheri', 'Kava', 'Korinn', 'Mishann', 'Nala', 'Perra', 'Raiann', 'Sora', 'Surina', 'Thava', 'Uadjit');
+		this.childNames.push('Climber', 'Earbender', 'Leaper', 'Pious', 'Shieldbiter', 'Zealous');
+		this.clanNames.push(' Clethtinthiallor', 'Daardendrian', 'Delmirev', 'Drachedandion', 'Fenkenkabradon', 'Kepeshkmolik', 'Kerrhylon', 'Kimbatuul', 'Linxakasendalor', 'Myastan', 'Nemmonis', 'Norixius', 'Ophinshtalajiir', 'Prexijandilin', 'Shestendeliath', 'Turnuroth', 'Verthisathurgiesh', 'Yarjerit')
+		this.str += 2;
+		this.cha += 1;
+		this.size = 'Medium';
+		this.speed = 30;
+		this.raceFeatures.push('Draconic Ancestry', 'Breath Weapon', 'Damage Resistance');
+		this.raceLanguages.push('Common', 'Draconic');
+		if (!firePreference) {
+			this.fireType = dragonbornFires[Math.floor(Math.random() * dragonbornFires.length)];
+		}
+		this.randomAge(adultPreference,81,14,2);
+		this.randomName(14);
+
+	}
+}
 
 
+class Dwarf extends Race{
+	toolProficiencies=["Smith's Tools","Brewer's Supplies","Mason's Tools"];
+	constructor(playerName,adultPreference,completeRandom,toolPreference){
+		super(playerName,completeRandom);
+		this.maleNames.push('Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal');
+		this.femaleNames.push('Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldeth', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', 'Riswynn', 'Sannl', 'Torbera', 'Torgga', 'Vistra');
+		this.clanNames.push('Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frostbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgehr', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart' );
+		this.randomAge(adultPreference,350,18,10);
+		this.randomName(0);
+		this.con+=2;
+		this.size='Medium';
+		this.speed=25;
+		this.raceFeatures.push('No reduction in speed by heavy armour','Darkvision','Dwarven Resilience');
+		this.raceProficiencies[1].push('Battleaxe','Handaxe','Light Hammer','Warhammer');
+		this.languages.push('Common','Dwarvish');
+		if (toolPreference === undefined) {
+			const tool =this.toolProficiencies[Math.floor(Math.random()*this.toolProficiencies.length)];
+			this.raceProficiencies[0].push(tool);
+		} else {
+			this.raceProficiencies[0].push(toolPreference);
+		}
+	}
+}
+
+class hillDwarf extends Dwarf{
+	constructor(playerName,adultPreference,completeRandom,toolPreference){
+		super(playerName,adultPreference,completeRandom,toolPreference);
+	}
+}
+
+class mountainDwarf extends Dwarf{
+	constructor(playerName,adultPreference,completeRandom,toolPreference){
+		super(playerName,adultPreference,completeRandom,toolPreference);
+	}
 }
